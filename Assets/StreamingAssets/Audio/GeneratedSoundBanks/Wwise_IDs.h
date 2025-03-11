062ea76f7bad1ae697d0512947152a9178b05748
David Su <david.d.su@gmail.com>
2025-03-11 02:00:32 -0700

    Generate soundbanks

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -16,6 +16,9 @@ namespace AK
         static const AkUniqueID PLAY_AMB_CITY_LP = 584628495U;
         static const AkUniqueID PLAY_AMB_FOREST_LP = 149137005U;
         static const AkUniqueID PLAY_AMB_INTERIOR_LP = 93749488U;
+        static const AkUniqueID PLAY_FLY_FOOTSTEP = 958544762U;
+        static const AkUniqueID PLAY_FLY_FOOTSTEP_CONCRETE = 3826579360U;
+        static const AkUniqueID PLAY_FLY_FOOTSTEP_GRASS = 4240657477U;
         static const AkUniqueID PLAY_IMP_PICKUP = 3369882767U;
         static const AkUniqueID PLAY_IMP_WALL = 2242885151U;
         static const AkUniqueID PLAY_MVT_BALLROLLING_LP = 4193143717U;
@@ -25,11 +28,27 @@ namespace AK
         static const AkUniqueID STOP_AMB_INTERIOR_LP = 2159561926U;
     } // namespace EVENTS
 
+    namespace SWITCHES
+    {
+        namespace SWI_FOOTSTEPMATERIAL
+        {
+            static const AkUniqueID GROUP = 3045844422U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace SWI_FOOTSTEPMATERIAL
+
+    } // namespace SWITCHES
+
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID RTPC_BALLSPEED = 3709966389U;
         static const AkUniqueID RTPC_COLLISIONVELOCITYNORMALIZED = 1573010119U;
         static const AkUniqueID RTPC_PLAYERDISTANCE = 3045223865U;
+        static const AkUniqueID RTPC_PLAYERSPEED = 2653406601U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
